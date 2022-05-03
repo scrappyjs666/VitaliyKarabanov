98738d16a51fa3aed6c8f9434add93e713875133
scrappyjs666 <82704685+scrappyjs666@users.noreply.github.com>
2022-05-03 17:07:43 +0300

    1

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,14 @@
-const {
-  resolve
-} = require('path')
-const {
-  defineConfig
-} = require('vite')
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
 
 module.exports = defineConfig({
   root: 'src',
-  base: '/nft-layout/dist1/',
+  base: '/test/dist/',
   build: {
     outDir: '../dist',
     rollupOptions: {
       input: {
         'index': resolve(__dirname, 'src/index.html'),
-        'best-seller': resolve(__dirname, 'src/best-seller.html'),
-        'Discover': resolve(__dirname, 'src/Discover.html'),
-        'Howitwork': resolve(__dirname, 'src/Howitwork.html'),
-        'Productdetalis': resolve(__dirname, 'src/Productdetalis.html')
       }
     }
   }
